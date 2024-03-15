f13eee1e05cbc6ae03119a6609f649fd48b74f2f
ghost2023 <nathnaelwonisha@gmail.com>
2024-03-15 22:13:00 +0300

    feat: update the sub

--- src/app/api/trigger/route.ts
+++ src/app/api/trigger/route.ts
@@ -17,12 +17,12 @@ export function GET(req: NextRequest) {
   // This is the same output of calling JSON.stringify on a PushSubscription
   const pushSubscription = {
     endpoint:
-      "https://fcm.googleapis.com/fcm/send/dS-byQBUQwY:APA91bHE0jA43-CnCcwCLkrHcErV0aLm6eobaz9MLDoMquRx_E-1qZwJY46BP_W5ig5yH4AlPBW19kvIlaemMXseLmB0ZB1lumEtR2w8e0qRkb3fDNvZOpC3rzrS9Ex_J3MLd_Fe3XIJ",
+      "https://fcm.googleapis.com/fcm/send/cN1IPBfUrbk:APA91bGCxoEMrU0Sjx-EqV8bmks-HllyerxikqIZ35Pbk4QtWrtD9w6-8QocmISb7nhQDabJ_b9Ky7jTZREQ6LuogTumeOL4Kjy-6z3R4vrWKHMMPFDxC8nfyPQcIRRen0Nt9VZmUrkx",
     expirationTime: null,
     keys: {
       p256dh:
-        "BAYDpR5AT40BPc1a-5NJXO1hbd8zpDXsigazMvKU7vIUF6mbxrRJbreR3z9BFQJN1knLyDxr7rqaf_EFbGQtVcY",
-      auth: "3ShMAucZ7xqno7mfMpldsA",
+        "BLU06z_fR51HBKHjrHStGXG-cpKwYT2h3kUcZ1wUJIQZ-uY9k4jvvpVih75pstNnlZQudpARlf7CRsVNZlQAPf4",
+      auth: "kBEM1ggz2tGJH5E9vKoFpg",
     },
   };
 
