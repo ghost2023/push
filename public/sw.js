23c540ac2bd8e59675dc3ae8c3e870fca28f0bc5
ghost2023 <nathnaelwonisha@gmail.com>
2024-03-15 22:10:26 +0300

    feat: update sw

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,13 @@
+self.addEventListener("install", function (event) {
+  event.waitUntil(self.skipWaiting()); // Activate worker immediately
+});
+
+self.addEventListener("activate", function (event) {
+  // This will be called only once when the service worker is activated.
+  console.log("Registerd Serviceworker");
+  event.waitUntil(self.clients.claim()); // Become available to all pages
+});
+
 self.addEventListener("push", () => {
   self.registration.showNotification("Hello world!");
 });
